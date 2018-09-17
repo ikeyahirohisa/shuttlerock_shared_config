74bb692aa36fbbe0c6f57a5ad1a016ed30703433
tbvokh <tbvokh@gmail.com>
2018-09-17 18:23:18 +0300

    [#13989] Added support for js project

---
+++ update_all.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+// .eslintrc will be created or overwritten by default.
+fs.copyFile(__dirname+'/templates/.eslintrc', '.eslintrc', (err) => {
+  if (err) throw err;
+  console.log('.eslintrc was successfully copied');
+});
