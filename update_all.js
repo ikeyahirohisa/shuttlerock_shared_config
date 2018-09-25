32e3e196dc67502b5021e376c942b3885723206d
tbvokh <tbvokh@gmail.com>
2018-09-25 10:31:58 +0300

    [#13989] Updated .eslintrc; Added script for updating .eslintrc

--- update_all.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// .eslintrc will be created or overwritten by default.
-fs.copyFile(__dirname+'/templates/.eslintrc', '.eslintrc', (err) => {
-  if (err) throw err;
-  console.log('.eslintrc was successfully copied');
-});
